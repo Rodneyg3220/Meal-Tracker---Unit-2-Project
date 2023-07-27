2d033eab6709e17bff710e542a21d649f7b76364
Rodneyg3220 <rodneygivens9@gmail.com>
2023-07-27 17:20:46 -0400

    latest commit

--- routes/meals.js
+++ routes/meals.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 // You'll be creating this controller module next
 const mealsCtrl = require('../controllers/meals');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 router.get('/', mealsCtrl.index);
 
-router.get('/new', mealsCtrl.newMeals);
+router.get('/new', ensureLoggedIn, mealsCtrl.newMeals);
 
 router.get('/:id', mealsCtrl.show);
 
-router.post('/', mealsCtrl.create);
+router.post('/', ensureLoggedIn, mealsCtrl.create);
 
 router.delete("/:id", mealsCtrl.delete);
 
