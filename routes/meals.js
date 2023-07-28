b976ae95c46bb7ec135e1af9cc5bea65a4aa1774
Rodneyg3220 <137218767+Rodneyg3220@users.noreply.github.com>
2023-07-28 09:59:09 -0400

    Update meals.js

--- routes/meals.js
+++ routes/meals.js
@@ -6,13 +6,13 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // GET /meals
 router.get('/', mealsCtrl.index);
-// GET /movies/new
+// GET /meals/new
 router.get('/new', ensureLoggedIn, mealsCtrl.newMeals);
 // GET /meals/:id (show functionality) 
 router.get('/:id', mealsCtrl.show);
 // POST /meals
 router.post('/', ensureLoggedIn, mealsCtrl.create);
-
+// DELETE /meals/:id
 router.delete("/:id", mealsCtrl.delete);
 
 
